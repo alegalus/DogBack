ffc4f7885aea69cbe3a83ab3a0cf68d041489a52
alegalus <alejandrogalus83@yahoo.com.ar>
2023-01-20 23:20:24 -0300

    conexion con el puerto de railways

--- index.js
+++ index.js
@@ -20,11 +20,13 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
-const port = process.env.PGPORT
+const { PGPORT } = process.env
+
+
 
 // Syncing all the models at once.
 conn.sync().then(() => {
-  server.listen(port, () => {
-    console.log('%s listening at ' + port); // eslint-disable-line no-console
+  server.listen(PGPORT, () => {
+    console.log('%s listening at ' + PGPORT); // eslint-disable-line no-console
   });
 });
